dbe6abab425b7f54cdde00f4b96c700e1433aeae
Brandon Weber <brandon@basistheory.com>
2022-12-06 16:09:21 -0600

    Add google tag manager

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,6 +41,10 @@ const config = {
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
+        gtag: {
+          trackingID: 'GTM-M56229L',
+          anonymizeIP: true,
+        },
       }),
     ],
   ],
