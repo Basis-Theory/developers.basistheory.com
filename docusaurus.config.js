a451c00a649da7569305fcfa695e4cccea272097
kevin <kevin@basistheory.com>
2022-12-06 10:16:08 -0600

    style: run prettier

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,46 +1,45 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
 
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'Basis Theory Developers',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://developers.basistheory.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
+  title: "Basis Theory Documentation",
+  url: "https://developers.basistheory.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'basis-theory', // Usually your GitHub org/user name.
-  projectName: 'developers.basistheory.com', // Usually your repo name.
+  organizationName: "basis-theory", // Usually your GitHub org/user name.
+  projectName: "developers.basistheory.com", // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/basis-theory/developers.basistheory.com/tree/master/',
+            "https://github.com/basis-theory/developers.basistheory.com/tree/master/",
         },
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       }),
     ],
@@ -51,73 +50,73 @@ const config = {
     ({
       navbar: {
         logo: {
-          alt: 'Basis Theory Logo',
-          src: 'img/bt-logo-light.svg',
-          srcDark: 'img/bt-logo-dark.svg',
-          href: '/',
-          target: '_self',
+          alt: "Basis Theory Logo",
+          src: "img/bt-logo-light.svg",
+          srcDark: "img/bt-logo-dark.svg",
+          href: "/",
+          target: "_self",
         },
         items: [
           {
-            type: 'doc',
-            docId: 'index',
-            position: 'left',
-            label: 'Docs',
+            type: "doc",
+            docId: "index",
+            position: "left",
+            label: "Docs",
           },
           {
-            type: 'doc',
-            docId: 'api',
-            position: 'left',
-            label: 'API',
+            type: "doc",
+            docId: "api",
+            position: "left",
+            label: "API",
           },
           {
-            type: 'doc',
-            docId: 'sdks',
-            position: 'left',
-            label: 'SDKs',
+            type: "doc",
+            docId: "sdks",
+            position: "left",
+            label: "SDKs",
           },
           {
-            href: 'https://github.com/basis-theory',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://github.com/basis-theory",
+            label: "GitHub",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Docs",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs',
+                label: "Tutorial",
+                to: "/docs",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Community",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/docusaurus",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Discord",
+                href: "https://discordapp.com/invite/docusaurus",
               },
               {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
+                label: "Twitter",
+                href: "https://twitter.com/docusaurus",
               },
             ],
           },
           {
-            title: 'More',
+            title: "More",
             items: [
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "GitHub",
+                href: "https://github.com/facebook/docusaurus",
               },
             ],
           },
@@ -127,7 +126,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ['csharp'],
+        additionalLanguages: ["csharp"],
       },
     }),
 };
