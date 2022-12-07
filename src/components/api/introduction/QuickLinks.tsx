411ee03b11ac96aaa0182224b1bcb45115003ee9
Brandon Weber <brandon@basistheory.com>
2022-12-07 08:14:46 -0600

    update links on API quickstart

--- src/components/api/introduction/QuickLinks.tsx
+++ src/components/api/introduction/QuickLinks.tsx
@@ -5,19 +5,18 @@ import styles from "./QuickLinks.module.css";
 export const QuickLinks = () => (
   <div className={styles.container}>
     <div>
-      <h2>API Reference</h2>
       <h3>Quick Links</h3>
-      <a className={styles.link} href="#getting-started">
-        Before You Begin
-      </a>
-      <a className={styles.link} href="/docs/api/tokens">
+      <a className={styles.link} href="/docs/api/tokens#create-token">
         Creating Tokens
       </a>
       <a className={styles.link} href="/docs/api/applications">
         Applications
       </a>
       <a className={styles.link} href="/docs/api/applications/permissions">
-        API Keys
+        Permissions
+      </a>
+      <a className={styles.link} href="/docs/api/proxies/invoke-proxy">
+        Send Data
       </a>
     </div>
     <div>
