4a386a24f856d9aad26f8860801d638c4a54649a
kevin <kevin@basistheory.com>
2022-12-09 20:03:54 -0600

    fix: add prod url to navbar signin button

--- src/components/navbar/SignInButton.tsx
+++ src/components/navbar/SignInButton.tsx
@@ -3,7 +3,7 @@ import { Button } from "../shared/Button";
 
 export const SignInButton = () => (
   <Button
-    href="https://portal-dev.basistheory.com/"
+    href="https://portal.basistheory.com/"
     target="_blank"
     variant="secondary"
   >
