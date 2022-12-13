920a0a2b7622aba7cff8901cf7d446f5842b0c4b
kevin <kevin@basistheory.com>
2022-12-13 17:21:31 -0600

    fix: button improvements

--- src/components/navbar/SignInButton.tsx
+++ src/components/navbar/SignInButton.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { Button } from "../shared/Button";
 
+import styles from "./SignInButton.module.css";
+
 export const SignInButton = () => (
   <Button
     href="https://portal.basistheory.com/"
     target="_blank"
     variant="secondary"
+    className={styles.button}
   >
     Sign In
   </Button>
