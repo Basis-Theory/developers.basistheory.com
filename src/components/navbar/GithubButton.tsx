0d30ee6965a774e0f9e7fb8f6d8686e7e041665c
kevin <kevin@basistheory.com>
2022-12-20 15:02:25 -0600

    feat: add icons to sidebar
    fix: relocate github and signin buttons to mobile sidebar header

--- src/components/navbar/GithubButton.tsx
+++ src/components/navbar/GithubButton.tsx
@@ -1,11 +1,12 @@
 import Github from "@site/static/img/shared/github.svg";
+import clsx from "clsx";
 import React from "react";
 
 import styles from "./GithubButton.module.css";
 
-export const GithubButton = () => (
+export const GithubButton = ({ className }) => (
   <a
-    className={styles.github}
+    className={clsx([styles.github, className])}
     href="https://github.com/basis-theory"
     target="_blank"
   >
