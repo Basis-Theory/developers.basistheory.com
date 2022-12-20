3c92b482bb494c6c21f38ae22ad2594cfec83263
kevin <kevin@basistheory.com>
2022-12-19 18:31:13 -0600

    fix: address color toggle icons console warnings

--- src/theme/Icon/DarkMode/index.js
+++ src/theme/Icon/DarkMode/index.js
@@ -15,37 +15,37 @@ export default function IconDarkMode({ hover, ...other }) {
       <path
         d="M17.5 11.5V10"
         stroke={colors.highlight}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M16.75 10.75H18.25"
         stroke={colors.highlight}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M21.25 12.25V15.25"
         stroke={colors.highlight}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M22.75 13.75H19.75"
         stroke={colors.highlight}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M20.05 19C18.175 19.6 16 19.225 14.5 17.725C12.625 15.85 12.475 12.85 13.975 10.75C10.9 11.05 8.5 13.6 8.5 16.75C8.5 20.05 11.2 22.75 14.5 22.75C16.975 22.75 19.15 21.175 20.05 19Z"
         stroke={colors.primary}
-        stroke-width="1.5"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
