03c40c2bb6a6d5fe9560418b8326e24bea888070
Josue Leon Sarkis <jolesa97@gmail.com>
2023-01-19 14:25:27 -0600

    feat: adding android collect guide (#35)

--- sidebars.js
+++ sidebars.js
@@ -40,6 +40,7 @@ const sidebars = {
       items: [
         "guides/collect/collect-data-from-web",
         "guides/collect/collect-data-with-react",
+        "guides/collect/collect-data-with-android",
         "guides/collect/collect-data-with-ios",
         "guides/collect/collect-inbound-data-to-api",
         "guides/collect/customize-web-form",
