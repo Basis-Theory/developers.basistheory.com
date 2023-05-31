56b81e607305c9b849a7c201aafbc7548681858c
Drew Hudec <drew@basistheory.com>
2023-05-31 09:41:58 -0400

    Adds beta tags to transaction pages

--- src/components/shared/Pill.tsx
+++ src/components/shared/Pill.tsx
@@ -3,4 +3,7 @@ import React, { PropsWithChildren } from "react";
 import styles from "./Pill.module.css";
 import clsx, { ClassValue } from "clsx";
 
-export const Pill = ({ children, className }: PropsWithChildren<{ className?: ClassValue }>) => <span className={clsx(styles.pill, className)}>{children}</span>;
+export const Pill = ({ children, className }: PropsWithChildren<{ className?: ClassValue }>) =>
+  <div className={clsx(styles["pill-container"])}>
+    <span className={clsx(styles.pill, className)}>{children}</span>
+  </div>;
