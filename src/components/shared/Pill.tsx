6d29e6bcb271b31bc585b574a09fc9cb945d01f4
James Armstead <james@basistheory.com>
2023-08-10 13:13:57 -0500

    feat: adding reserved headers to proxies

--- src/components/shared/Pill.tsx
+++ src/components/shared/Pill.tsx
@@ -3,7 +3,7 @@ import React, { PropsWithChildren } from "react";
 import styles from "./Pill.module.css";
 import clsx, { ClassValue } from "clsx";
 
-export const Pill = ({ children, className }: PropsWithChildren<{ className?: ClassValue }>) =>
+export const Pill = ({ children, className, title }: PropsWithChildren<{ className?: ClassValue, title?: string }>) =>
   <div className={clsx(styles["pill-container"])}>
-    <span className={clsx(styles.pill, className)}>{children}</span>
+    <span className={clsx(styles.pill, className)} title={title}>{children}</span>
   </div>;
