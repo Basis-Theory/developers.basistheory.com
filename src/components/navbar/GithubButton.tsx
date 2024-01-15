d01ec8c5d957356eb164f3c288eacd3b729a5381
kevin <kevin@basistheory.com>
2024-01-15 08:41:34 -0600

    style: run prettier

--- src/components/navbar/GithubButton.tsx
+++ src/components/navbar/GithubButton.tsx
@@ -5,11 +5,7 @@ import React from "react";
 import styles from "./GithubButton.module.css";
 
 export const GithubButton = ({ className }) => (
-  <a
-    className={clsx([styles.github, className])}
-    href="https://github.com/basis-theory"
-    target="_blank"
-  >
+  <a className={clsx([styles.github, className])} href="https://github.com/basis-theory" target="_blank">
     <Github />
   </a>
 );
