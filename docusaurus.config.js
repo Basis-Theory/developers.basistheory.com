5fbb651916cc4580e94f702c0ebe950f3ff0f2f7
Robert G. Greathouse <robert@basistheory.com>
2024-11-25 08:11:15 -0600

    Remove API legacy tab

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -81,12 +81,6 @@ const config = {
           },
           {
             type: "doc",
-            docId: "api-legacy/index",
-            position: "left",
-            label: "API (legacy)",
-          },
-          {
-            type: "doc",
             docId: "sdks/index",
             position: "left",
             label: "SDKs",
