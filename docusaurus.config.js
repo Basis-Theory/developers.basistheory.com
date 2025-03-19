b4cd878ffa05345bb2b6f83926ebc58d15ed8a80
James Armstead <james@basistheory.com>
2025-03-19 09:52:58 -0500

    feat: adding the llm-full.txt

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,5 +1,7 @@
 // @ts-check
 // Note: type annotations allow type checking and IDEs autocompletion
+import fs from "node:fs";
+import path from "node:path";
 
 const lightCodeTheme = require("./prism-custom-themes/light");
 const darkCodeTheme = require("./prism-custom-themes/dark");
@@ -273,6 +275,76 @@ const config = {
         ],
       },
     ],
+    async function pluginLlmsTxt(context) {
+      return {
+        name: "llms-txt-plugin",
+        loadContent: async () => {
+          const { siteDir } = context;
+          const contentDir = path.join(siteDir, "docs");
+          const allMdx = [];
+
+          // recursive function to get all mdx files
+          const getMdxFiles = async (dir) => {
+            const entries = await fs.promises.readdir(dir, { withFileTypes: true });
+
+            for (const entry of entries) {
+              const fullPath = path.join(dir, entry.name);
+              if (entry.isDirectory()) {
+                await getMdxFiles(fullPath);
+              } else if (entry.name.endsWith(".mdx")) {
+                const content = await fs.promises.readFile(fullPath, "utf8");
+                allMdx.push(content);
+              }
+            }
+          };
+
+          await getMdxFiles(contentDir);
+          return { allMdx };
+        },
+        postBuild: async ({ content, routes, outDir }) => {
+          const { allMdx } = content;
+
+          // Write concatenated MDX content
+          const concatenatedPath = path.join(outDir, "llms-full.txt");
+          await fs.promises.writeFile(concatenatedPath, allMdx.join("\n\n---\n\n"));
+
+          // we need to dig down several layers:
+          // find PluginRouteConfig marked by plugin.name === "docusaurus-plugin-content-docs"
+          const docsPluginRouteConfig = routes.filter(
+            (route) => route.plugin.name === "docusaurus-plugin-content-docs"
+          )[0];
+
+          // docsPluginRouteConfig has a routes property has a record with the path "/" that contains all docs routes.
+          const allDocsRouteConfig = docsPluginRouteConfig.routes?.filter(
+            (route) => route.path === "/"
+          )[0];
+
+          // A little type checking first
+          if (!allDocsRouteConfig?.props?.version) {
+            return;
+          }
+
+          // this route config has a `props` property that contains the current documentation.
+          const currentVersionDocsRoutes = allDocsRouteConfig.props.version.docs;
+
+          // for every single docs route we now parse a path (which is the key) and a title
+          const docsRecords = Object.entries(currentVersionDocsRoutes).map(([path, record]) => {
+            return `- [${record.title}](${path}): ${record.description}`;
+          });
+
+          // Build up llms.txt file
+          const llmsTxt = `# ${context.siteConfig.title}\n\n## Docs\n\n${docsRecords.join("\n")}`;
+
+          // Write llms.txt file
+          const llmsTxtPath = path.join(outDir, "llms.txt");
+          try {
+            fs.writeFileSync(llmsTxtPath, llmsTxt);
+          } catch (err) {
+            throw err;
+          }
+        },
+      };
+    },
     // https://github.com/facebook/docusaurus/issues/8297
     // https://github.com/svg/svgo/issues/1714
     // @ts-ignore
@@ -294,7 +366,18 @@ const config = {
           if (svgRuleIndex === -1 || svgrConfigIndex === -1) return;
 
           // @ts-ignore
-          config.module.rules[svgRuleIndex].oneOf[svgrConfigIndex].use[0].options.svgoConfig.plugins[0].params.overrides.cleanupIDs = false;
+          config.module.rules[svgRuleIndex].oneOf[svgrConfigIndex].use[0].options.svgoConfig = {
+            plugins: [
+              {
+                name: 'preset-default',
+                params: {
+                  overrides: {
+                    cleanupIds: false
+                  }
+                }
+              }
+            ]
+          };
         },
       };
     },
